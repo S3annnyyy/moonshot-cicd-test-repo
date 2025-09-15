7d535e2797e832959157f4f834f81e97c6c2c31f
S3annnyyy <seanyckang@gmail.com>
2025-09-15 11:53:11 +0800

    test setup

---
+++ app.py
@@ -0,0 +1,30 @@
+import re 
+from quart import Quart, jsonify, request
+from quart_cors import cors
+
+app = Quart(__name__)
+app = cors(app, allow_origin=re.compile(r"http:\/\/localhost(:[0:9]+)?"))
+
+
+@app.route("/", methods=["GET"])
+async def health_check():
+    return jsonify({"code": 200, "response": "backend service is up and running"})
+
+
+@app.route("/api/v1/conversation", methods=["POST"])
+async def conversation():
+    data = await request.get_json()
+
+    message = data.get('message')
+    return jsonify({"data": f"received message: {message}"}), 200
+
+
+if __name__ == "__main__":
+    app.run(port=3123)
+
+# For local testing
+'''
+curl -X http:localhost:3123/api/v1/conversation \
+     -H "Content-Type": application/json \
+     -d '{"message": "I want to find out more about the technology sector"}'
+'''
